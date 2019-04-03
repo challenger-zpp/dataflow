0fe035f62490ac2a8161e2aa586266922b057153
unknown <hustlidi@126.com>
2019-04-03 17:26:16 +0800

    add daily quote of index

--- api_dailyquote.py
+++ api_dailyquote.py
@@ -53,3 +53,36 @@ def get_daily_quote(stock_code,start_date,end_date):
     quote_data=quote_data.sort_values('TradeDate',ascending = True)
     
     return quote_data
+
+
+def get_daily_quote_index(index_code,start_date,end_date):
+    '''
+    获取指数日线行情。
+    
+    Parameters
+    ----------
+    stock_code
+        指数代码
+    star_date
+        开始日期 yyyymmdd
+    endd_ate
+        结束日期 yyyymmdd
+        
+    Returns
+    -------
+    DataFrame
+        若无数据,则返回None
+    '''       
+    
+    
+    sql_str='''
+    SELECT 
+    TradeDate,StockCode,[Open],[Close],High,Low,Vol,Amount  
+    FROM BasicData.dbo.DailyQuote_Index  
+    WHERE StockCode='%s' and TradeDate>='%s' and TradeDate<='%s'
+    '''%(index_code,start_date,end_date)
+    
+    quote_data= get_raw_data(sql_str)
+    quote_data=quote_data.sort_values('TradeDate',ascending = True)
+    
+    return quote_data
